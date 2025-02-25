103d7011c69d0dcfb9ed25df2ab61a4eee458ab2
seniya <seniyathri@gmail.com>
2025-02-25 11:45:53 +0100

    first commit

---
+++ src/main/java/pizzeria/it/piazzamvc/PiazzamvcApplication.java
@@ -0,0 +1,13 @@
+package pizzeria.it.piazzamvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiazzamvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PiazzamvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pizzeria/it/piazzamvc/PiazzamvcApplicationTests.java
@@ -0,0 +1,13 @@
+package pizzeria.it.piazzamvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PiazzamvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
